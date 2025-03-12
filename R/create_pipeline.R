6ca43df2a1bc8777fa24874f00f3118ff87fec00
Sam Herniman <samherniman@posteo.net>
2025-03-12 14:25:25 +0100

    Fixed typo

--- R/create_pipeline.R
+++ R/create_pipeline.R
@@ -22,7 +22,7 @@ create_pipeline <- function(output_dir) {
   del = lasR::triangulate(filter = lasR::keep_ground())
   dtm = lasR::rasterize(0.25, del, ofile = fs::path(output_dir, "dtm", "dtm.tif"))
   del2 = lasR::triangulate(filter = lasR::keep_first())
-  t_height = lasR::rasterize(c(2, 5), operators = "z_95", ofile = fs::path(output_dir, "height_baba", "height_baba.tif"))
+  t_height = lasR::rasterize(c(2, 5), operators = "z_p95", ofile = fs::path(output_dir, "height_baba", "height_baba.tif"))
   chm = lasR::rasterize(0.5, del2)
   chm2 = lasR::pit_fill(chm, ofile = fs::path(output_dir, "chm", "chm.tif"))
 
