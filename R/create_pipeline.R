6d97a9a486b072896c0608761413fea3b5f12715
Sam Herniman <samherniman@posteo.net>
2025-03-13 15:26:41 +0100

    experiment with rasterize

--- R/create_pipeline.R
+++ R/create_pipeline.R
@@ -22,7 +22,7 @@ create_pipeline <- function(output_dir) {
   del = lasR::triangulate(filter = lasR::keep_ground())
   dtm = lasR::rasterize(0.25, del, ofile = fs::path(output_dir, "dtm", "dtm.tif"))
   del2 = lasR::triangulate(filter = lasR::keep_first())
-  baba_ras = lasR::rasterize(c(1, 5), operators = c("z_p95", "z_above10", "z_above3"), ofile = fs::path(output_dir, "baba", "baba.tif"))
+  baba_ras = lasR::rasterize(c(2, 5), operators = c("z_p95", "z_above10", "z_above3"), ofile = fs::path(output_dir, "baba", "baba.tif"))
   chm = lasR::rasterize(0.5, del2)
   chm2 = lasR::pit_fill(chm, ofile = fs::path(output_dir, "chm", "chm.tif"))
 
